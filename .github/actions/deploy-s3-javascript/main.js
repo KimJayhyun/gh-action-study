c50c99ece6d568a4e17f1069cacb3ffceb6298e4
KimJayhyun <dnlwjem000@gmail.com>
2025-05-11 18:32:42 +0900

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -17,5 +17,9 @@ function run() {
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
   core.notice("Hello from my custom Javascript Action!");
+
+  // 3) Output website URL
+  const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteURL);
 }
 run();
